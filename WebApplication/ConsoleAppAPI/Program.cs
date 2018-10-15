676f47e396c92353d9840a15ede664da46df47b1
Tanaykatarey <102191142@student.swin.edu.au>
2018-10-15 16:38:41 +1100

    Week 10 Commint

---
+++ WebApplication/ConsoleAppAPI/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ConsoleAppAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Run Task!");
+            Task.Run(TaskAsync).Wait();
+            Console.ReadLine();
+        }
+
+        static async Task TaskAsync()
+        {
+            Console.WriteLine("enter yall name");
+            string name = Console.ReadLine();
+            Console.WriteLine("email plz hehe");
+            string email = Console.ReadLine();
+            Console.WriteLine("what ya wanna say?");
+            string message = Console.ReadLine();
+
+            HttpClient client = new HttpClient();
+            var StringContent = new StringContent(JsonConvert.SerializeObject(new Details(name, email, message)), System.Text.Encoding.UTF8, "application/json");         
+            var result = await new HttpClient().PostAsync("http://localhost:49981/api/Contact", StringContent);
+            var str = await result.Content.ReadAsStringAsync();
+        }
+
+        public class Details
+        {
+            public string Name { get; set; }
+            public string Email { get; set; }
+            public string Message { get; set; }
+
+            public Details(string name, string email, string message)
+            {
+                this.Name = name;
+                this.Email = email;
+                this.Message = message;
+            }
+
+        }
+    }
+}
---
+++ WebApplication/WebApplication/Controllers/ContactController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Contact")]
+    public class ContactController : Controller
+    {
+        [HttpPost]
+        public IActionResult Post([FromBody] ContactPostRequestModel model)
+        {
+            return Ok(new
+            {
+                DateTime = DateTime.Now,
+                Model = model
+            });
+
+        }
+
+        public class ContactPostRequestModel
+        {
+            public string Name { get; set; }
+            public string Email { get; set; }
+            public string Message { get; set; }
+
+        }
+
+
+    }
+}
